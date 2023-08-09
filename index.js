96ea3db46602e03f67b4835a02c264ac8af4e2d6
Artyom Tsybulkin <artyomtsybulkin@outlook.com>
2023-08-10 00:07:53 +0400

    Commenting code

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 import { Sandway } from './modules/sandway.mjs';
 import { Template } from './modules/sandway.mjs';
 
+/* 1. Initialize Class with single property: routes */
 const app = new Sandway();
 
+/* 2. While execting this method add middleware property to Class instance
+   In this case middleware is arrow function */
 app.use((req, res) => {
     console.log(req.method, req.url);
 });
 
 app.get('/', (req, res) => {
+    /* 3. route = '/' and handler is arrow function
+        Next perform actions like in express - 8 in sandway.mjs. */
     req.on('data', (data) => {
         console.log('Data received: %s', data);
     });
-
-    const templateString = '<h1>Hello, {{name}}!</h1><p>Welcome to our website.</p>';
+    /* Working on templates here */
+    const templateString = 'Hello, {{name}}! Welcome to our website.';
     const template = new Template(templateString);
     const data = {
         name: 'John'
@@ -24,6 +29,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res, queryParams) => {
+    /* 4. Same way as for GET request - 8 in sandway.mjs. */
     req.on('data', (data) => {
         console.log('Data received: %s', JSON.parse(data));
     });
@@ -31,9 +37,11 @@ app.post('/', (req, res, queryParams) => {
     res.end(`Received POST request with query parameters: ${JSON.stringify(queryParams)}`);
 });
 
+/* 5. Return http.createServer instance */
 const server = app.createServer();
 const PORT = process.env.PORT || 3000;
 
+/* 5. Listen is inher3ted from node http module */
 server.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
--- modules/sandway.mjs
+++ modules/sandway.mjs
@@ -4,28 +4,41 @@ import { parse as _parse } from 'querystring';
 
 class Sandway {
 	constructor() {
+		/* 1. Initialize Class with single property:
+		routes - empty object */
     	this.routes = {};
   	}
 	use(middleware) {
+		/* 2. While execting this method add middleware
+		property to Class instance */
     	this.middleware = middleware;
   	}
 	get(route, handler) {
+		/* 3. Method to add property "'GET ' + route" with
+		value "handler()" to routes object */
     	this.routes['GET ' + route] = handler;
   	}
 	post(route, handler) {
+		/* 4. Same way as for GET request */
     	this.routes['POST ' + route] = handler;
   	}
 	createServer() {
+		/* 5. Return http.createServer instance */
     	return _createServer((req, res) => {
 			const method = req.method;
 			const parsedUrl = parse(req.url);
 			const routeKey = `${method} ${parsedUrl.pathname}`;
 			const queryParams = _parse(parsedUrl.query);
+			/* 6. If middleware defined then execute */
 			if (this.middleware) {
 				this.middleware(req, res);
 			}
+			/* 7. Then handle requests:
+			routes[routeKey] - defined by get and/or post methods */
 			if (this.routes[routeKey]) {
 				const handler = this.routes[routeKey];
+				/* 8. Call handler as value of property routes[routeKey].
+				Note: this call will execute code in index.js */
 				handler(req, res, queryParams);
 			} else {
 				res.writeHead(404, { 'Content-Type': 'text/plain' });
